0335a8fb04ee092e023eca87bf41c031ecf32c37
Jeffcaruso <50027834+Jeffcaruso@users.noreply.github.com>
2023-12-07 16:41:49 -0800

    Fixed build error issue. Also continued pre-setup for tests
    - fixes in gtest (yaml for GHub actions)
    - tests removing the incorrect classes, and unneeded test files
    - building.cpp is now at least close to where it needs to be

--- Tests/Test4.cpp
+++ Tests/Test4.cpp
@@ -1,33 +1,20 @@
 //============================================================================
 // Name        : Test4
-// Test Desc.  : Test multiplication
-//				 	(coverage for old test 4 method)
+// Test Desc.  : <desc>
 // Author      : Jeffrey Caruso
 // Date    	   : Fall 2023
 //============================================================================
 
 #include <gtest/gtest.h>
-#include "applib/largenum.h"
+#include "applib/maze.h"
 
 #include <iostream>
 #include <sstream>
 
 using namespace std;
 
-TEST(Test4, MultiplicationTesting)
+TEST(Test4, updateName)
 {
-	// is true that 25 * 0 = 0
-	EXPECT_TRUE(LargeNum(25) * LargeNum(0) == LargeNum(0));
+	Maze myMaze;
 
-	// is true that 25 * 5 = 125
-	EXPECT_TRUE(LargeNum(25) * LargeNum(5) == LargeNum(125));
-
-	//is true that (-25) * (-5) = 125
-	EXPECT_TRUE(LargeNum(-25) * LargeNum(-5) == LargeNum(125));
-
-	// is true that (-25) * (5) = -125
-	EXPECT_TRUE(LargeNum(-25) * LargeNum(5) == LargeNum(-125));
-
-	// is true that 25 * (-5) = -125
-	EXPECT_TRUE(LargeNum(25) * LargeNum(-5) == LargeNum(-125));
 }
