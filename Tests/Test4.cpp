91781aa8e94ac05d2db28bc2f09f4d906134d4ed
Jeffcaruso <50027834+Jeffcaruso@users.noreply.github.com>
2023-12-08 19:50:48 -0800

    Tests started for Test 2,3,4. (Test 1 was completed)

--- Tests/Test4.cpp
+++ Tests/Test4.cpp
@@ -16,6 +16,34 @@ using namespace std;
 TEST(Test4, updateName)
 {
 	Maze myMaze;
+    //string mazeFile = "maze0.txt";
+    string mazeFile = "data/maze3.txt";
+    bool result = false;
+    result = myMaze.load(mazeFile);
+    if (! result)
+    {
+        cerr << "Failed to load " << mazeFile << endl;
+        //rest of this is useless without the test file being loaded in, so exit here...
+        ASSERT_TRUE(result); 
+        return;
+    }
+    //if made it here, should pass.
+    EXPECT_TRUE(result); 
+
+    cout << "*** Solving " << mazeFile << endl;
+    result = myMaze.solve();
+    if (result)
+    {
+        EXPECT_TRUE(result);
+        cout << "Path: " << myMaze.getPath() << endl;
+    }
+    else
+    {
+        //should fail here... b/c we failed to solve it.
+        EXPECT_TRUE(result);
+        cout << "Failed to solve: " << mazeFile << endl;
+    }
+    cout << myMaze << endl;
 
 }
 
