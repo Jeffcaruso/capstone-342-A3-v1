c3098167e172b0092fb48229f577c1b88958c215
Jeffcaruso <50027834+Jeffcaruso@users.noreply.github.com>
2023-12-08 18:14:42 -0800

    Fixes to test 1. Also, I think at this point, I can call setup complete

--- Tests/Test1.cpp
+++ Tests/Test1.cpp
@@ -17,44 +17,30 @@ TEST(Test1, updateName)
 {
 	Maze myMaze;
     string mazeFile = "maze0.txt";
-    if (!myMaze.load(mazeFile))
+    bool result = false;
+    result = myMaze.load(mazeFile);
+    if (! result)
     {
         cerr << "Failed to load " << mazeFile << endl;
-        return -1;
+        //rest of this is useless without the test file being loaded in, so exit here...
+        ASSERT_TRUE(result); 
+        return;
     }
+    //if made it here, should pass.
+    EXPECT_TRUE(result); 
+
     cout << "*** Solving " << mazeFile << endl;
-    if (myMaze.solve())
+    result = myMaze.solve();
+    if (result)
     {
+        EXPECT_TRUE(result);
         cout << "Path: " << myMaze.getPath() << endl;
     }
     else
     {
+        //should fail here... b/c we failed to solve it.
+        EXPECT_TRUE(result);
         cout << "Failed to solve: " << mazeFile << endl;
     }
     cout << myMaze << endl;
 }
-
-//going to need to automate and explain something like this in gTest (for maze0)
-    // Maze myMaze;
-
-    // //don't need to loop through tests. That coverage is being done by gTest.
-    // //can loop through one tho...
-
-    // string mazeFile = "maze0.txt";
-    // if (!myMaze.load(mazeFile))
-    // {
-    //     cerr << "Failed to load " << mazeFile << endl;
-    //     return -1;
-    // }
-    // cout << "*** Solving " << mazeFile << endl;
-    // if (myMaze.solve())
-    // {
-    //     cout << "Path: " << myMaze.getPath() << endl;
-    // }
-    // else
-    // {
-    //     cout << "Failed to solve: " << mazeFile << endl;
-    // }
-    // cout << myMaze << endl;
-
-//This cannot be easily split without changing the actual tests becuase they depend on each number...
